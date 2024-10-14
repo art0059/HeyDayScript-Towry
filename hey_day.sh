cebf4613a1d589598df504520d7d2095100c1f3f
art0059 <art0059@auburn.edu>
2024-10-14 17:06:26 -0500

    Add files via upload

---
+++ hey_day.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# You're walking on the concourse to class, you meet Aubie the Tiger, and he hands you a name tag
+echo "Fill out the name tag: Hello! My name is _________"
+read name
+
+# The student beside you sees your name tag and greets you
+echo "Hey $name! I hope you have an awesome Hey Day! It was great to meet you, War Eage!" 
